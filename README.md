155b325beba3bd0760bc2ce137caa1bca0486dbf
Richard Kafati <richardkafati@gmail.com>
2015-01-16 01:16:00 -0600

    event.preventDefault(); not working: food.js line 14 and ui sticky not working: food.html

---
+++ README.md
@@ -0,0 +1,5 @@
+This app displays available foods and allows foods to be added, updated or deleted.
+
+Clicking on a food opens a modal view where food properties can be modified, however, the modal's form isn't executing event.preventDefault(); in food.js, line 14; why not? This is my main problem.
+
+In food.html the "ui sticky" is yielding the error "Sticky element is larger than its container, cannot create sticky." to the console and I'm also not sure why?
--- client/templates/food.js
+++ client/templates/food.js
@@ -1,7 +1,27 @@
-Template.pageGrid.events({
+Template.food.events({
 
   "click .delete": function () {
     Foods.remove(this._id);
+  },
+
+  "click .openModal": function () {
+    foodNameWithoutSpaces = this.food.replace(/\s+/g, '');
+    $('#' + foodNameWithoutSpaces + 'ModalView')
+      .modal('show');
+  },
+
+  "submit .update-food": function () {
+    event.preventDefault();
+    console.log("trying to log BUT prevent default not working.");
+    console.log(event);
+  }
+
+});
+
+Template.food.helpers({
+
+  "foodNameWithoutSpaces" : function (food) {
+    return food.replace(/\s+/g, '');
   }
 
 });
